fbe3cf113311f500809a5da6782f33f609774d5f
sami <dcil142@samiti.speedport.ip>
2021-07-18 20:08:54 +0200

    to make them responsive by adding beside key in Grid

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ const App = () => {
         <Container className={classes.cardGrid} maxWidth='md'>
           <Grid container spacing={4}>
             {cards.map((card) => (
-              <Grid item key={card}>
+              <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
