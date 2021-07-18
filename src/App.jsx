2acffeb8b178fb00bb898c14ab39e75b768091fd
sami <dcil142@samiti.speedport.ip>
2021-07-18 20:05:32 +0200

    using map function in order to loop the images instead of adding 9 images

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ import {
 
 import PhotoCamera from '@material-ui/icons/';
 import useStyles from './styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const App = () => {
   const classes = useStyles();
   return (
@@ -67,32 +70,34 @@ const App = () => {
         </div>
         <Container className={classes.cardGrid} maxWidth='md'>
           <Grid container spacing={4}>
-            <Grid item>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image='https://source.unsplash.com/random'
-                  title='Image title'
-                />
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant='h5'>
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is a media card. you can use this section to describe
-                    the content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size='small' color='primary'>
-                    View
-                  </Button>
-                  <Button size='small' color='primary'>
-                    Edit
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
+            {cards.map(() => (
+              <Grid item>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image='https://source.unsplash.com/random'
+                    title='Image title'
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant='h5'>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. you can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size='small' color='primary'>
+                      View
+                    </Button>
+                    <Button size='small' color='primary'>
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
         </Container>
       </main>
