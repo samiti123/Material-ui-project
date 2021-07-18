d94d49887ada88a0c7222f518adf237c995e3b6c
sami <dcil142@samiti.speedport.ip>
2021-07-18 19:37:31 +0200

    transferring from app to styles.js by creating the file

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,7 @@ import {
 } from '@material-ui/core';
 
 import PhotoCamera from '@material-ui/icons/';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  container: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-}));
-
+import useStyles from './styles';
 const App = () => {
   const classes = useStyles();
   return (
