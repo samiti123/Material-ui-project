4ad5e811560f737a81dbca29dd0da5b9ea5ebf2c
sami <dcil142@samiti.speedport.ip>
2021-07-18 20:00:10 +0200

    adding styles for cardGrid,card,cardMedia and cardContent

--- src/styles.js
+++ src/styles.js
@@ -11,6 +11,20 @@ const useStyles = makeStyles((theme) => ({
   buttons: {
     marginTop: '40px',
   },
+  cardGrid: {
+    padding: '20px 0',
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%',
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
 }));
 
 export default useStyles;
